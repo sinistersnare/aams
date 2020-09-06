abcce9646404ddebb0dc9c17c116474bd1abcb5f
Davis Silverman <sinistersnare@gmail.com>
2020-09-05 22:59:13 -0400

    Make aceskt* an actual k-cfa???

--- src/aceskt-star.rkt
+++ src/aceskt-star.rkt
@@ -3,80 +3,127 @@
 (require (only-in racket/random random-ref))
 (require (only-in racket/hash hash-union))
 
-; An aCESKt* machine! The tick function is `identity` so this is a 0CFA?
+; An aCESKt* machine!
+; implements a k-CFA-like machine. defined by the AAM paper
+
+; This is a k-cfa, so this to the k you want.
+; is it really that easy? Seems like `k` is only used in the `tick` function.
+; nice!
+(define k-value 2)
 
 ; make a state of the machine.
-(define (make-state expr env env-store k-store cont-addr timestamp)
-  (list expr env env-store k-store cont-addr timestamp))
+(struct state (expr env env-store k-store k-addr timestamp) #:transparent)
+
+; a timestamp is a label and a contour
+(struct timestamp (label contour) #:transparent)
+; an address is a (label or variable) and a contour
+(struct address (label contour) #:transparent)
+
+
+; like `take` in the stdlib, but will return the
+; taken items so far if we reach the end of the list.
+(define (takesafe lst n)
+  (if (or (= n 0) (null? lst))
+      '()
+      (cons (car lst) (takesafe (cdr lst) (- n 1)))))
+
+(define (tick st kont)
+  (match st
+    [(struct state ((cons (? symbol? _) _) _ _ _ _ t)) t]
+    [(struct state ((cons `(,e0 ,e1) elabel) _ _ _ _ (struct timestamp (tlabel contour))))
+     (timestamp elabel contour)]
+    [(struct state ((cons `(λ ,xvar ,ebody) elabel) _ _ _ _ (struct timestamp (label contour))))
+     (match kont
+       ; HELP: intereting that timestamp doesnt change here. Why?
+       ; need to understand this whole thing a bit better.
+       [(list 'ar _ _ _) (timestamp label contour)]
+       ; add the label to the contour list, and then take the floor of it.
+       ; the empty list (bullet/nil from AAM) is only allowed in label position,
+       ; not contour position. Could cause issues!
+       [(list 'fn _ _ _) (timestamp '() (takesafe (cons label contour) k-value))]
+       [else (raise `(bad-kont-tick st: ,st kont: ,kont))])]
+    [else (raise `(bad-tick-args st: ,st kont: ,kont))]))
+
+(define (alloc st kont)
+  (match st
+    [(struct state ((cons `(,(cons e0 e0label) ,e1) _) _ _ _ _ (struct timestamp (_ contour))))
+     (address e0label contour)]
+    [(struct state ((cons `(λ ,_ ,_) _) _ _ k-store k-addr (struct timestamp (_ contour))))
+     (match kont
+       ; HELP: What goes here?
+       ['mt (address 'mt contour)]
+       [`(ar ,(cons _ elabel) ,_ ,_) (address elabel contour)]
+       [`(fn (λ ,x ,_) ,_ ,_) (address x contour)]
+       [else (raise `(bad-kont-alloc st: ,st kont: ,kont))])]
+    [else (raise `(bad-alloc-args st: ,st kont: ,kont))]))
 
 ; create an initial state around a closed expression
 (define (inj-aceskt* e)
+  (define a0 (address '0 '()))
+  (define t0 (timestamp '() '()))
   ; expr , env , env-store , k-store , k
   ; where the stores are Addr -> P(ValueType). 
-  (make-state e (hash) (hash) (hash 0 (set 'mt)) 0 1))
+  (state e (hash) (hash) (hash a0 (set 'mt)) a0 t0))
 
 ; move the machine 1 step from a given state.
 ; takes a single state, returns a list of states that can be reached.
-(define (step-aceskt* state)
-  ; right now, tick is just defined as the identity function
-  ; but it needs a continuation argument, and it needs to be not the identity function
-  ; what will it be?
-  ; HELP: above.
-  (define (tick t) t)
-  (println `(stepping: ,state))
-  (match-define (list expr env env-store k-store cont-addr timestamp) state)
+(define (step-aceskt* st)
+  (println `(stepping: ,st))
+  (match-define (struct state (expr env env-store k-store k-addr timestamp)) st)
   (match expr
-    [(? symbol? x)
-     ; i think if we add first-class continuations (so then continuations could be in the env-store)
-     ; then the 2-store method would become annoying, and we would just go back to structure checking
-     ; structure checking as in, we need access to the continuation,
-     ; it could be any variable (becauase we are storing them alongside eachother in sets)
-     ; so the solution would be ensure that the `k` is a continuation, or just use 2 stores.
-     (define res-set (hash-ref env-store (hash-ref env x)))
-     ; anyways, res-set is a set of (v pprime) lists.
-     ; we need to return new states based on each element of res-set.
-     (set-map res-set (lambda (res) (make-state (car res) (cdr res) env-store
-                                                k-store cont-addr (tick timestamp))))]
-    [`(,e0 ,e1)
+    [(cons (? symbol? x) label)
+     ; current-xs is all (v pprime) pairs at the address `x`.
+     ; current-ks is all continuations at the address `k-addr`
+     ; we need to return new states based on each combo of those two.
+     (define current-xs (set->list (hash-ref env-store (hash-ref env x))))
+     (define current-ks (set->list (hash-ref k-store k-addr)))
+     (define products (cartesian-product current-xs current-ks))
+     (map (lambda (product)
+            (match-define `(,(cons val storeprime) ,k) product)
+            (state val storeprime env-store k-store k (tick st k)))
+          products)]
+    [(cons `(,e0 ,e1) label)
      ; current-ks is all continuations at the current continuation address.
-     (define current-ks (hash-ref k-store cont-addr))
+     (define current-ks (hash-ref k-store k-addr))
      ; HELP: So alloc is `alloc(varsigma, k) where k is all current-ks
      ; but im not sure what to do with `k` in the alloc func,
      ; update:
      ; im just gonna use the k itself as the alloc-address!!
      ; it seems like itll work! But I have no idea what working is!
      ; so the mt entry is 0 -> mt. Then the next one would be
-     ; mt -> ar(e1 env cont-addr)... thats weird.
+     ; mt -> ar(e1 env k-addr)... thats weird.
      (set-map current-ks
-              (lambda (k-addr)
-                (define new-cont `(ar ,e1 ,env ,cont-addr))
-                (make-state e0 env env-store (hash-update k-store k-addr
-                                                          (lambda (k-set) (set-add k-set new-cont))
-                                                          (lambda () (set new-cont)))
-                            k-addr (tick timestamp))))]
-    [`(λ ,xarg ,ebody)
-     (define current-ks (hash-ref k-store cont-addr))
+              (lambda (current-k)
+                (define b-addr (alloc st current-k))
+                (define new-cont `(ar ,e1 ,env ,k-addr))
+                (state e0 env env-store (hash-update k-store b-addr
+                                                     (lambda (k-set) (set-add k-set new-cont))
+                                                     (lambda () (set new-cont)))
+                       b-addr (tick st current-k))))]
+    [(cons `(λ ,xarg ,ebody) label)
+     (define current-ks (hash-ref k-store k-addr))
      (set-map
       current-ks
-      (lambda (k-addr)
-        (match k-addr
+      (lambda (current-k)
+        (define b-addr (alloc st current-k))
+        (match current-k
           ; at a fixpoint, return state
-          ['mt state]
+          ['mt st]
           [`(ar ,e ,pprime ,c)
-           (define new-cont `(fn ,expr ,env ,c))
-           (make-state e pprime env-store (hash-update k-store k-addr
-                                                       (lambda (k-set) (set-add k-set new-cont))
-                                                       (lambda () (set new-cont)))
-                       k-addr (tick timestamp))]
+           (define new-cont `(fn (λ ,xarg ,ebody) ,env ,c))
+           (state e pprime env-store (hash-update k-store b-addr
+                                                  (lambda (k-set) (set-add k-set new-cont))
+                                                  (lambda () (set new-cont)))
+                  b-addr (tick st current-k))]
           [`(fn (λ ,x ,e) ,pprime ,c)
-           (define env-item (cons expr env))
-           (make-state e (hash-set pprime x k-addr)
-                       (hash-update env-store k-addr
-                                    (lambda (env-set) (set-add env-set env-item))
-                                    (lambda () (set env-item)))
-                       k-store c (tick timestamp))]
-          [else (raise `(bad-cont: ,k-addr))])))]
-    [else (raise (list 'bad-syntax expr env env-store k-store cont-addr))]))
+           (define env-item (cons `(λ ,xarg ,ebody) env))
+           (state e (hash-set pprime x b-addr)
+                  (hash-update env-store b-addr
+                               (lambda (env-set) (set-add env-set env-item))
+                               (lambda () (set env-item)))
+                  k-store c (tick st current-k))]
+          [else (raise `(bad-cont: ,current-k))])))]
+    [else (raise (list 'bad-syntax expr env env-store k-store k-addr))]))
 
 ; evaluate from an initial state
 ; this works differently from non-abstract abstract-machines, because here
@@ -85,6 +132,13 @@
 ; HELP: But isnt the defn of fixpoint `x where f(x) = x`?
 ; how is 'reached before already' a fixpoint? Am I visualizing the step function wrong?
 (define (evaluate e)
+  ; add labels to `e`.
+  (define (label e)
+    (match e
+      [(? symbol? x) (cons x (gensym 'lab))]
+      [`(,e0 ,e1) (cons `(,(label e0) ,(label e1)) (gensym 'lab))]
+      [`(λ ,x ,e) (cons `(λ ,x ,(label e)) (gensym 'lab))]
+      [else `(labeling-error ,e)]))
   ; go takes a list of states to process and a hash of reached states (and where they lead)
   ; and returns a set of reached states, and where they lead (like an edge-list).
   (define (go states reached)
@@ -97,10 +151,11 @@
     ; shoudnt be hit by dupe errors, as we check when calculating todo-states for possible dupes.
     (define union-reached (hash-union reached new-reached))
     ; use union-reached instead of reached because when we reach a fixpoint we need to catch it.
-    (define todo-states (filter-not (lambda (s) (hash-has-key? union-reached s))
-                                    (append-map identity (hash-values new-reached))))
+    ; TODO: swap args and use `dropf` instead of filter-not. Feel like itll read better.
+    (define todo-states (dropf (append-map identity (hash-values new-reached))
+                               (lambda (s) (hash-has-key? union-reached s))))
     (if (empty? todo-states) union-reached (go todo-states union-reached)))
-  (go (list (inj-aceskt* e)) (hash)))
+  (go (list (inj-aceskt* (label e))) (hash)))
 
 (define edges (evaluate '((λ n (λ s (λ z (s ((n s) z))))) (λ s (λ z z)))))
 
