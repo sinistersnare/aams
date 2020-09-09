7912c5e2cc094a2bbbc8b1b317a17a10c6caf9b1
Davis Silverman <sinistersnare@gmail.com>
2020-09-09 01:35:54 -0400

    Fix bugs in CFA, add helper function for translating online lambda-calc

--- src/aceskt-star.rkt
+++ src/aceskt-star.rkt
@@ -9,7 +9,7 @@
 ; This is a k-cfa, so this to the k you want.
 ; is it really that easy? Seems like `k` is only used in the `tick` function.
 ; nice!
-(define k-value 2)
+(define k-value 0)
 
 ; make a state of the machine.
 (define (state expr env env-store k-store k-addr timestamp)
@@ -22,7 +22,6 @@
 ; an address is a (label or variable) and a contour
 (define (address label contour)
   (list 'address label contour))
-
 
 ; like `take` in the stdlib, but will return the
 ; taken items so far if we reach the end of the list.
@@ -54,11 +53,10 @@
      (address e0label contour)]
     [(list 'state (cons `(λ ,_ ,_) _) _ _ k-store k-addr (list 'timestamp _ contour))
      (match kont
-       ; HELP: What goes here?
        ['mt (address 'mt contour)]
        [`(ar ,(cons _ elabel) ,_ ,_) (address elabel contour)]
-       [`(fn (λ ,x ,_) ,_ ,_) (address x contour)]
-       [else (raise `(bad-kont-alloc st: ,st kont: ,kont))])]
+       [`(fn ,(cons `(λ ,x ,_) (? symbol?)) ,_ ,_) (address x contour)]
+       [else (println kont) (raise `(bad-kont-alloc st: ,st kont: ,kont))])]
     [else (raise `(bad-alloc-args st: ,st kont: ,kont))]))
 
 ; create an initial state around a closed expression
@@ -74,7 +72,7 @@
 (define (step-aceskt* st)
   (match-define (list 'state expr env env-store k-store k-addr timestamp) st)
   (match expr
-    [(cons (? symbol? x) label)
+    [(cons (? symbol? x) (? symbol? label))
      ; current-xs is all (v pprime) pairs at the address `x`.
      ; current-ks is all continuations at the address `k-addr`
      ; we need to return new states based on each combo of those two.
@@ -83,9 +81,9 @@
      (define products (cartesian-product current-xs current-ks))
      (map (lambda (product)
             (match-define `(,(cons val storeprime) ,k) product)
-            (state val storeprime env-store k-store k (tick st k)))
+            (state val storeprime env-store k-store k-addr (tick st k)))
           products)]
-    [(cons `(,e0 ,e1) label)
+    [(cons `(,e0 ,e1) (? symbol? label))
      ; current-ks is all continuations at the current continuation address.
      (define current-ks (hash-ref k-store k-addr))
      (set-map current-ks
@@ -96,23 +94,22 @@
                                                      (lambda (k-set) (set-add k-set new-cont))
                                                      (lambda () (set new-cont)))
                        b-addr (tick st current-k))))]
-    [(cons `(λ ,xarg ,ebody) label)
+    [(cons `(λ ,xarg ,ebody) (? symbol? label))
      (define current-ks (hash-ref k-store k-addr))
      (set-map
       current-ks
       (lambda (current-k)
         (define b-addr (alloc st current-k))
         (match current-k
-          ; at a fixpoint, return state
-          ['mt st]
+          ['mt st] ; at a fixpoint, return state
           [`(ar ,e ,pprime ,c)
-           (define new-cont `(fn (λ ,xarg ,ebody) ,env ,c))
+           (define new-cont `(fn ,(cons `(λ ,xarg ,ebody) label) ,env ,c))
            (state e pprime env-store (hash-update k-store b-addr
                                                   (lambda (k-set) (set-add k-set new-cont))
                                                   (lambda () (set new-cont)))
                   b-addr (tick st current-k))]
-          [`(fn (λ ,x ,e) ,pprime ,c)
-           (define env-item (cons `(λ ,xarg ,ebody) env))
+          [`(fn ,(cons `(λ ,x ,e) (? symbol? fnlabel)) ,pprime ,c)
+           (define env-item (cons (cons `(λ ,xarg ,ebody) fnlabel) env))
            (state e (hash-set pprime x b-addr)
                   (hash-update env-store b-addr
                                (lambda (env-set) (set-add env-set env-item))
@@ -153,11 +150,24 @@
     (if (empty? todo-states) union-reached (go todo-states union-reached)))
   (go (list (inj-aceskt* (label e))) (hash)))
 
-(define edges (evaluate '((λ n (λ s (λ z (s ((n s) z))))) (λ s (λ z z)))))
+;(define edges (evaluate '((λ n (λ s (λ z (s ((n s) z))))) (λ s (λ z z)))))
+(define edges (evaluate '((λ x (x x)) (λ x (x x)))))
+; (step-aceskt* (state '(λ x (((x . lab1) (x . lab2)) . lab3)) '() '() '() '() '()))
+; (step-aceskt* (state '(λ x (((x . lab99679) (x . lab99680)) . lab99681)) '() '() '() '() '()))
 
+; use the formatting module!
 #;(println "digraph G {")
 #;(hash-for-each
  edges
  (lambda (src dests)
    (for-each (lambda (dest) (println (string-append src " -> " dest))) dests)))
 #;(println "}")
+
+
+; found some λ-examples with this syntax, so ez transformation to mine.
+(define (unwrap e)
+  (match e
+    [(? symbol? x) x]
+    [`(,efn ,earg) `(,(unwrap efn) ,(unwrap earg))]
+    [`(λ (,(? symbol? fnarg)) ,e) `(λ ,fnarg ,(unwrap e))]))
+(define (eu e) (evaluate (unwrap e)))
